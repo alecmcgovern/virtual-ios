a9eff01f632123f8e811ab4133a1cff5842a483a
Alec M <alecmcgovern444@gmail.com>
2018-01-31 17:22:59 -0500

    initial exress server set up

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server);
+
+const PORT = process.env.PORT || 8000;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+server.listen(PORT, () => {
+	console.log('video app is listening on port ' + PORT);
+});
