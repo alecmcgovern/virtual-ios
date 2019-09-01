24a3cbbdb9bd6aa6d759207da1e0d7bc22851020
Alec <alecmcgovern444@gmail.com>
2019-09-01 14:06:36 -0400

    redirect to https:

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const io = require('socket.io')(server);
 
 const PORT = process.env.PORT || 8000;
 
+app.use(requireHTTPS);
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
@@ -77,6 +78,14 @@ io.on('connection', function(client) {
 		}
 	});
 });
+
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
 
 server.listen(PORT, () => {
 	console.log('video app is listening on port ' + PORT);
